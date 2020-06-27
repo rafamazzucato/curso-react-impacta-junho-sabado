38d682210b79a58181c36841256a324151d40fd2
Rafael Thomazelli <rafael@verzel.com.br>
2020-06-27 13:15:28 -0300

    commit servidor http

--- aula01/node/app.js
+++ aula01/node/app.js
@@ -4,37 +4,37 @@ const {exibirMensagem}  = require('./modulo2');
 mod1('Meu primeiro módulo');
 exibirMensagem('Meu segundo módulo');
 
-const
-let
-var
+// const
+// let
+// var
 
-var v = 'variavel que existe no escopo global, ou seja, todos podem visualizar e alterar ela';
-let l = 'variavel que existe dentro de um escopo, ou seja, sómente dentro daquele escopo a variavel poderá ser acessada e alterada'
-const c = 'constanta que existe dentro de um escopo';
+// var v = 'variavel que existe no escopo global, ou seja, todos podem visualizar e alterar ela';
+// let l = 'variavel que existe dentro de um escopo, ou seja, sómente dentro daquele escopo a variavel poderá ser acessada e alterada'
+// const c = 'constanta que existe dentro de um escopo';
 
-const f = function(x){
-    v = 'alterar o v';
-    for(let i = 0; i < 10000000000; i++){
-        if(i === 3){
-            console.log(3);
-            break;
-        }
-    }
+// const f = function(x){
+//     v = 'alterar o v';
+//     for(let i = 0; i < 10000000000; i++){
+//         if(i === 3){
+//             console.log(3);
+//             break;
+//         }
+//     }
 
-    console.log(i);
-}
+//     console.log(i);
+// }
 
-const obj = {
-    teste : 'teste',
-    funcao : () => {}
-}
+// const obj = {
+//     teste : 'teste',
+//     funcao : () => {}
+// }
 
-const {funcao} = obj
+// const {funcao} = obj
 
-if( true && true && true && true){
-    if(){
-        if(){
+// if( true && true && true && true){
+//     if(){
+//         if(){
 
-        }
-    }
-}
+//         }
+//     }
+// }
---
+++ aula01/node/server/http.js
@@ -0,0 +1,17 @@
+// requisita o módulo HTTP do Node.js
+const http = require('http');
+
+// cria a função que vai tratar as requisições do servidor de forma genérica
+const requisicaoPadrao = (_, resposta) => {
+    resposta.writeHead(200, { 'Content-Type': 'text/html'});
+    resposta.write('<h1>Texto a ser exibido no browser</h1>');
+    resposta.end();
+}
+
+// cria o servidor com o tratamento das requisições feitas pela nossa aplicação
+const server = http.createServer(requisicaoPadrao);
+
+// roda o servidor na porta 3000 e ao concluir a subida, informa via função anônima
+server.listen(3000, _ => {
+    console.log('Servidor no ar na porta 3000');
+});
